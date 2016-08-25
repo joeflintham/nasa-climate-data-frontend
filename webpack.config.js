45d72b83b1f5e52a6c08830bdee859c857f11f7d
Joe Flintham <joeflintham@gmail.com>
2016-08-26 00:12:23 +0100

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 var path = require('path');
 module.exports = {
-    entry: './lib/main.js',
-    output: {
-        path: path.join(__dirname, 'app/scripts/lib'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: path.join(__dirname, 'lib'),
-                query: {
-                    presets: 'es2015',
-                },
-            }
-        ]
-    }
+  entry: './lib/main.js',
+  output: {
+    path: path.join(__dirname, 'app/scripts/lib'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        exclude: [
+          path.resolve(__dirname, "bower_components")
+        ],
+        test: path.join(__dirname, 'lib'),
+        query: {
+          presets: 'es2015',
+        }
+      },
+      {
+        loader: 'inject-loader',
+        test: path.join(__dirname, 'lib')
+      }
+    ]
+  }
 };
