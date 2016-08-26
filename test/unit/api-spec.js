2a6dce4281de4c7e74d2dd2ad5e0bf30fb3f7503
Joe Flintham <joeflintham@gmail.com>
2016-08-26 16:00:08 +0100

    Spy on XHR for API unit test

--- test/unit/api-spec.js
+++ test/unit/api-spec.js
@@ -1,30 +1,48 @@
 import { describe, before, it } from 'mocha';
 import {assert, expect, should} from 'chai';
 import mockery from 'mockery';
+import { spy, stub } from 'sinon';
+
+const openSpy = spy();
+const sendSpy = spy();
 
 const XHRMock = function () {};
 XHRMock.prototype = {
-  onload: function() {console.log(arguments);}
+  open: openSpy,
+  send: sendSpy
 };
+
+mockery.enable({
+  warnOnUnregistered: false
+});
+mockery.registerMock('./xhr.js', XHRMock);
+
+const API = require('../../lib/api.js');
 
 describe('api module', function () {
 
-  let API;
-  
-  beforeEach(function () {
-    mockery.enable();
-    mockery.warnOnUnregistered(false);
-    mockery.warnOnReplace(false);
-    mockery.registerMock("./xhr.js", XHRMock);
-    API = require('../../lib/api.js');
-  });
-  
   it('should be defined', function () {
     assert.equal(typeof API, "object");
   });
 
+  it('should handle missing co-ordinates', function () {
+    assert.equal(typeof API.GetByCoords, 'function');
+    assert.equal(API.GetByCoords(), undefined);
+  });
+
+  let coords = {
+    lat: 51.1,
+    lng: 1.1
+  };
+  
   it('should get data by co-ordinates', function () {
-    assert.equal(typeof API.default.GetByCoords, 'function');
-    assert.equal(API.default.GetByCoords(), undefined);
+    assert.equal(typeof API.GetByCoords, 'function');
+
+    let apiCall = API.GetByCoords(coords);
+    expect(apiCall instanceof Promise)
+    expect(openSpy.called).to.equal(true);
+    expect(sendSpy.called).to.equal(true);
+
+    Promise.resolve(apiCall);
   });
 });
